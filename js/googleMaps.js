cf3cf2a82f8e35d019aaca2699615c471c9c476c
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-01-23 13:31:57 -0500

    added SVGs

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,84 +1,103 @@
-//=========================================================================================================//
 
-//---------------------------------------------Google Maps-------------------------------------------------//
 
-//=========================================================================================================//
+var GoogleMapApi = (function(){
 
-var GoogleMapApi = (function(options) {
+   var shared = {};
+    
+  var map, infoWindow;
 
-    var map, infoWindow;
-    var markers = [];
+  var centerPoint = {
+      lat: 32.7799072,
+      lng: -79.9337208
+  };
 
-    function initMap() {
-        var centerPoint = {
-            lat: 32.7799072,
-            lng: -79.9337208
-        };
+  function initMap() {
+    infowindow = new google.maps.InfoWindow();
 
-        infowindow = new google.maps.InfoWindow();
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: centerPoint,
-            zoom: 13
-        });
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: centerPoint,
+      zoom: 13
+    });
+
+    var image = {
+        url: '../assets/img/home.png',
+        // This marker is 32 pixels wide by 32 pixels high.
+        size: new google.maps.Size(32, 32),
+        // The origin for this image is (0, 0).
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        anchor: new google.maps.Point(15, 32)
+    };
+    
+    var marker = new google.maps.Marker({
+      position:centerPoint,
+      map: map,
+      icon: image,
+      animation: google.maps.Animation.DROP
+    });
+    
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('My Airbnb');
+        infowindow.open(map, this);
+    });
+  };
+  
+  function createMarker(result) {
+    var marker = new google.maps.Marker({
+      position: result.geometry.location,
+      map: map,
+      title: result.name,
+      animation: google.maps.Animation.DROP
+    });
+    
+    google.maps.event.addListener(marker, 'click', function() {
+        createInfoWindow(result, marker);
+        infowindow.open(map, this);
+    });
+  };
+
+  function createInfoWindow(result, marker) {
+    var contentString = `<h3 class="marker-title">${result.title}<h3>`
+    infowindow.setContent(contentString);
+  };
+
+
+  function search() {
+    var searchItem = $("form input[name=query]").value;
+    var request = {
+      location: centerPoint,
+      radius: '5',
+      query: searchItem,
+      openNow:true
     };
 
-   
-    function createMarker(result, info) {
-        var marker = new google.maps.Marker({
-            position: result.geometry.location,
-            map: map,
-            animation: google.maps.Animation.DROP
-        });
-        
-        google.maps.event.addListener(marker, 'click', function() {
+    var service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, processPlacesResults);
+  }
 
-            GooglePlacesApi.search({
-                latitude: result.geometry.location.lat,
-                longitude: result.geometry.location.lng
-            });
+ 
+  $('form[name=search] button').click(function(event) {
+    event.preventDefault();
+    search();
+    console.log("form submitted");
+  });
 
+  function processPlacesResults(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        createMarker(result);
+      }
+    }
+  };
 
-            setInfoWindowContent(info);
-            infowindow.open(map, this);
-        });
-        markers.push(marker);
-    };
+ 
 
 
     
-    function setMapOnAll(map) {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(map);
-        }
-    }
-
-    
-    function clearMarkers() {
-        setMapOnAll(null);
-    }
-
-    
-    function deleteMarkers() {
-        clearMarkers();
-        markers = [];
-    }
-
-    function setInfoWindowContent(info) {
-        var contentString = ``;
-
-        // if (info.title) contentString += `<h3 class="marker-title">${info.title}<h3>`;
-        // if (info.date) contentString += `<p class="show-date">${info.date}</p>`;
-        // if (info.link && info.link_img) contentString += `<a href="${info.link}" target="_blank" class="link"><img src="${info.link_img}"></a>`;
-        // if (info.other) contentString += `<div class="other">${info.description}</div>`;
-
-        // infowindow.setContent(contentString);
-    };
-
-    return {
-        init: initMap,
-        setMapOnAll: setMapOnAll,
-        createMarker: createMarker,
-        deleteMarkers: deleteMarkers
-    };
+  return {
+    init: initMap,
+    createMarker: createMarker
+  };
 
 }());
