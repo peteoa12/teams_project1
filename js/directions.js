38d74fe5035ca478b71fd48e63bc8a94dd32d762
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-02-04 15:41:45 -0500

    Added styling

--- js/directions.js
+++ js/directions.js
@@ -1,27 +1,18 @@
-GoogleDirectionsApi(function(){
+var GoogleDirectionsApi = (function(){
 
-	function setUpSearch() {
-
-		var url = 'https://maps.googleapis.com/maps/api/directions/outputFormat?parameters'
-		.$.ajax({
-			url: '/path/to/file',
-			type: 'default GET (Other values: POST)',
-			dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-			data: {param1: 'value1'},
-		})
-		.done(function() {
-			console.log("success");
-		})
-		.fail(function() {
-			console.log("error");
-		})
-		.always(function() {
-			console.log("complete");
-		});
+	function setUpSearch(data) {
 		
+		var url = 'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=AIzaSyDdjYkP3rGKMBl3m6N6qfT4F7FWUQCr-Yk';
+		$.ajax({
+			url: url,
+			method: 'GET',
+			dataType: 'json'
+		    }).done(function(data) {
+		        console.log("success", data);
+		    }).fail(function(jqXHR, textStatus, errorThrown) {
+		        console.log(jqXHR, textStatus, errorThrown)
+		    })
 	}
+	setUpSearch();
 
-	return {
-
-	}
-})
+}());
--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ var getExtraPlaceData = function() {
 	places.forEach(function(place) {
 		var gid = place.google_id;
 		GoogleMapApi.getPlaceById(gid, function( data ) {
-			// console.log("google api replied with", data, place);
 			place.google_data = data;
 			addPlaceToDom(place, data);
+		
 		});
 	})
 }
 
-var addPlaceToDom = function( place, data ) {
+var addPlaceToDom = function( place, data, photo ) {
 	console.log(data);
 
 	place.$element = $(
-	'<li class="result cafe"><img class="result_img" src="https://pbs.twimg.com/profile_images/608377553808703489/2gBSCjiq.jpg"><div class="result_copy"><span class="rating">'+data.rating+'</span><h2 class="result_title">'+data.name+'</h2><address class="result_address">'+data.formatted_address+'</address></div></li>')
+	`<li class="result cafe"><img class="result_img" src="https://pbs.twimg.com/profile_images/608377553808703489/2gBSCjiq.jpg"><div class="result_copy"><span class="rating">${data.rating}</span><h2 class="result_title">${data.name}</h2><address class="result_address">${data.formatted_address}</address></div></li>`)
 
 	$(".results_list").append(place.$element); //reference to the data structure.
 
@@ -33,12 +33,10 @@ function hideShow(){
 		places.forEach(function(place) {
 			if (!place.google_data) return;
 			if (place.placeType == placeType) {
-				// show this item in the list and on the map
 				console.log("show", place)
 				$(place.$element).show();
 				GoogleMapApi.showPin(place.google_data.marker);
 			} else {
-				// hide this item in list and on map
 				console.log("hide", place);
 				$(place.$element).hide();
 				GoogleMapApi.hidePin(place.google_data.marker);
@@ -47,8 +45,6 @@ function hideShow(){
 		 
 	});
 }
-
-
 
 var init = function() {
 	GoogleMapApi.init();
