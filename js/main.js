75f8720e75852bcdd84b3ff1d28aa9eccd0d0316
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-02-06 12:18:01 -0500

    finalizing

--- js/main.js
+++ js/main.js
@@ -45,9 +45,14 @@ var addPlaceToDom = function( place, data, photo ) {
                             </div>
                             <div class="cta">
                             	<div class="directions">
-                            		<span></span>
+                            		<span>Get Directions</span>
                             	</div>
-                            	<div class="uber"></div>
+                            	<div class="uber">
+	                            	<a  href="https://m.uber.com/ul/?client_id=SaBTN4Tn_n9tn1NTGy9OxR9fP2ii67YF&action=setPickup&pickup[latitude]=37.775818&pickup[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d&link_text=View%20team%20roster&partner_deeplink=partner%3A%2F%2Fteam%2F9383">
+	                            		<img class="uber_icon" src="assets/img/uber-icon.png">
+	                            		<h6>Call Uber</h6>
+	                            	</a>
+                            	</div>
                             </div>
                         </div>
                     </li>`)
