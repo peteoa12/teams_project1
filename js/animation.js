cb2e0257050f63057de79dc28997de136fff724e
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-03-16 13:51:02 -0400

    animation

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+
+	
+//icons
+TweenMax.staggerFrom(".icon", 0.2, {x:-50, alpha:0, delay:0.5, ease:Power3.easeIn}, 0.2); 
+
+//nav links
+TweenMax.staggerFrom(".nav_links", 0.4, {y:50, alpha:0, delay:0.5, ease:Power3.easeIn}, 0.2);
+
+//logo
+TweenMax.fromTo(".logo_container", 0.3, {y:50, opacity:0}, {y:0, opacity:1, ease:Power4.easeIn})
+
+
+
