1727924ffaeaab39dccb2350a9c8253a5b4e4214
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-03-16 14:08:52 -0400

    styling and animation

--- js/data.js
+++ js/data.js
@@ -4,63 +4,82 @@ var places = [
 		google_id: "ChIJY8eG9Qt6_ogR8_CLUdaHByk",
 		star: false,
 		placeType: "dining",
+		review: '"This is the best place ever! Try the smoked beatles!"',
+		photo:'https://resizer.otstatic.com/v2/photos/medium/23700930.jpg'
 		// Fleet Landing
 	},
 	{
 		id: 1,
 		google_id: "ChIJh_y8iw56_ogRbBIgxAsPnS8",
 		star: false,
-		placeType: "dining"
+		placeType: "dining",
+		review: '"My favorite dish is the shrimp scampie!"',
+		photo:'http://blog.swig.co/wp-content/uploads/2016/06/The-Traveller-Bar-side-image-and-Gin-Fix-cocktail-300x300.jpg'
 		//McCrady's Tavern
 	},
 	{
 		id: 2,
 		google_id: "ChIJzVr0vhF6_ogRpOuUEXBkl1c",
 		star: false,
-		placeType: "dining"
-		//Husk
+		placeType: "dining",
+		review: '"The cocktails are to die for!"',
+		photo: 'http://img1.southernliving.timeinc.net/sites/default/files/styles/etr_slider_square/public/image/2014/08/main/husk-nashville-tennessee-m.jpg?itok=4Rli0vyO&timestamp=1423091763'
 	},
 	{
 		id: 3,
 		google_id: "ChIJg-q9MRJ6_ogRhw9ue_d-hfY",
 		star: false,
-		placeType: "cafe"
+		placeType: "cafe",
+		review: '"Stop by for a cup of joe and a cheese scone!"',
+		photo:'https://static1.squarespace.com/static/57cf27d79f7456e123173ccb/57d33eeccd0f68a33d360cc0/57d33ef8cd0f68a33d360d12/1473462009717/cafepastries.jpg?format=300w'
 	},
 	{
 		id: 4,
 		google_id: "ChIJLRlgCQ16_ogRNwAOInFkGZ4",
 		star: false,
-		placeType: "cafe"
+		placeType: "cafe",
+		review: '"Great coffee and homebrewed kombucha!"',
+		photo:'http://cdn-image.myrecipes.com/sites/default/files/styles/300x300/public/image/recipes/ck/04/03/cafe-brulot-ck-592294-x.jpg?itok=_m2zKXJX'
 	},
 	{
 		id: 5,
 		google_id: "ChIJsa1utBR6_ogR2tPlEQW2Ur4",
 		star: false,
-		placeType: "attractions"
+		placeType: "attractions",
+		review: '"Great museum to take the kids to!"',
+		photo:'http://charlestonplacemeetings.com/wp-content/uploads/2011/12/ocha_350_ch_gibbes_museum_of_art01.jpg'
 	},
 	{
 		id: 6,
 		google_id: "ChIJyXu6WnF6_ogRUe3Z8p_vrsw",
 		star: false,
-		placeType: "attractions"
+		placeType: "attractions",
+		review: '"Great museum to take the kids to!"',
+		photo:'http://travelerofcharleston.com/wp-content/uploads/2015/09/5-300x300.jpg'
 	},
 	{
 		id: 7,
 		google_id: "ChIJVU26UBN6_ogRK1vWk1Vy8Kg",
 		star: false,
-		placeType: "nightlife"
+		placeType: "nightlife",
+		review: '"You can shake your groove thing here!"',
+		photo:'https://www.vegasnightlifecrawl.com/tickets-vegas-bachelorette/wp-content/uploads/2017/01/clubDance02-bachelorette-party-las-vegas-bachelor-party-las-vegas-free-drinks-vegas-no-cover-vegas-nightlife-vegas-club-crawl-fremont-street-300x300.jpg'
 	},
 	{
 		id: 8,
 		google_id: "ChIJhW9sym96_ogRppqBuNTHT9A",
 		star: false,
-		placeType: "gas"
+		placeType: "gas",
+		review: '"Always the lowest gas rates!"',
+		photo:'https://lh3.googleusercontent.com/eEm8uYqFqEVcavL4H7B6nxhs8gbqSQ6BAosrI1qhrPC04JA7GekxbSeP3opOx_IUsZTA=w300'
 	},
 	{
 		id: 9,
 		google_id: "ChIJg3uRMw16_ogRrs22isylJvQ",
 		star: false,
-		placeType: "grocery"
+		placeType: "grocery",
+		review: '"Everything you need and a 15 minute walk from the apartment!"',
+		photo:'http://cdn-jpg.allyou.com/sites/default/files/image/2014/01/300x300/i/2013/3/harris-teeter-store-m.jpg'
 	}
 	
 ]
