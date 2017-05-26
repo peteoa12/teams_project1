ade052d5a32b143ae7ea46a971fa1fd121e73603
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-05-25 22:28:04 -0400

    built login screen

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,14 @@
-
+//Places people
+// var hiddenOnLoad = [
+// 	'.icon'
+// ]
+// TweenMax.set(hiddenOnLoad, {alpha:0});
 	
 //icons
-TweenMax.staggerFrom(".icon", 0.2, {x:-50, alpha:0, delay:0.5, ease:Power3.easeIn}, 0.2); 
+TweenMax.staggerFrom(".icon", 0.2, {x:-50, alpha:0, delay:0.5, ease:Power3.easeIn}, 0.1); 
 
 //nav links
-TweenMax.staggerFrom(".nav_links", 0.4, {y:50, alpha:0, delay:0.5, ease:Power3.easeIn}, 0.2);
+TweenMax.staggerFrom(".nav_links", 0.4, {y:50, alpha:0, delay:0.5, ease:Power3.easeIn}, 0.1);
 
 //logo
 TweenMax.fromTo(".logo_container", 0.3, {y:50, opacity:0}, {y:0, opacity:1, ease:Power4.easeIn})
